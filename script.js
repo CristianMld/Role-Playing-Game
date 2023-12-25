3fbd1e159bdcff3d34f30baf7588ae158855d749
cristianml <100762826+CristianMld@users.noreply.github.com>
2023-12-25 20:18:43 -0300

    32 done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var xp;
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
