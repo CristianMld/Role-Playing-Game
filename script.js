19ae73039c3ec8f1294e4d21b6ad4f4de62db5db
cristianml <100762826+CristianMld@users.noreply.github.com>
2023-12-27 01:15:43 -0300

    60 done

--- script.js
+++ script.js
@@ -22,8 +22,12 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+  
+}
+
 function goStore() {
-    console.log("Going to store.");  
+    
 }
 
 function goCave() {
@@ -33,3 +37,36 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
+function update(location) {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
+    }
+];
