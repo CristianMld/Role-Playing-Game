abf3922cee896e18bce34f3c052ed48d36e7869b
cristianml <100762826+CristianMld@users.noreply.github.com>
2023-12-28 04:12:54 -0300

    120 done

--- script.js
+++ script.js
@@ -143,21 +143,43 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
     }
 ];
 
 function fightSlime() {
-    
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
-    
+    fighting = 1;
+    goFight();
 }
 
 function fightDragon() {
-    console.log("Fighting dragon.");
+    fighting = 2;
+    goFight();
 }
 
 function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+}
+
+function dodge() {
   
 }
